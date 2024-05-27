dfc92d482cf4c8dd4eca7270a19a4b9fa0138fc8
shubham64mehta <shubham64mehta@gmail.com>
2024-05-27 23:00:50 +0530

    understanding of props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import "./App.css";
+import Coreconcepts from "./components/coreconcept";
 import Header from "./components/header";
+import { CORE_CONCEPTS } from "./data/data";
 
 function App() {
   return (
     <div>
       <Header />
+      <main>
+        <section id="core-concepts">
+          <ul>
+            <Coreconcepts {...CORE_CONCEPTS[0]} />
+            <Coreconcepts {...CORE_CONCEPTS[1]} />
+            <Coreconcepts {...CORE_CONCEPTS[2]} />
+            <Coreconcepts {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
+      </main>
     </div>
   );
 }
